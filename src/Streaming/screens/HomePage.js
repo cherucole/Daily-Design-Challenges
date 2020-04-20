161e79d488cb1df641f979c946759d4197d19489
Cheruiyot Collins <cheruyotcheruyot@gmail.com>
2020-04-20 16:03:21 +0300

    added gradient overlay on image

--- src/Streaming/screens/HomePage.js
+++ src/Streaming/screens/HomePage.js
@@ -9,6 +9,8 @@ import {
   Image,
 } from 'react-native';
 import Entypo from 'react-native-vector-icons/Entypo';
+import Ionicons from 'react-native-vector-icons/Ionicons';
+import LinearGradient from 'react-native-linear-gradient';
 
 const HomePage = props => {
   return (
@@ -17,19 +19,35 @@ const HomePage = props => {
       <ImageBackground
         style={styles.backgroundImage}
         source={require('../assets/home.jpg')}>
-        <View style={styles.rowMenu}>
-          <Entypo name="list" size={45} color="white" />
-          <View style={styles.avatarContainer}>
-            <View style={styles.avatar}>
-              <Image
-                style={styles.avatarImage}
-                source={require('../assets/passport.png')}
-              />
+        <LinearGradient
+          ref={r => (this.gradiant = r)}
+          locations={[0, 1.0]}
+          colors={['rgba(0,0,0,0.10)', 'rgba(0,0,0,0.80)']}
+          style={styles.linearGradient}>
+          <View style={styles.rowMenu}>
+            <Entypo name="list" size={45} color="white" />
+            <View style={styles.avatarContainer}>
+              <View style={styles.avatar}>
+                <Image
+                  style={styles.avatarImage}
+                  source={require('../assets/passport.png')}
+                />
+              </View>
             </View>
           </View>
-        </View>
+          <Text style={styles.heroText}>Explore the</Text>
+          <Text style={styles.heroText}>world of games</Text>
+          <View style={styles.searchRow}>
+            <Ionicons name="ios-search" size={25} color="#bbdefb" />
+            <View style={styles.searchPlaceholder}>
+              <Text style={styles.searchText}>Search for any game</Text>
+            </View>
+          </View>
+        </LinearGradient>
       </ImageBackground>
+
       <Text>HomePage</Text>
+
       <Button
         title="Go To Details"
         onPress={() => props.navigation.navigate('Details')}
@@ -45,7 +63,7 @@ const styles = StyleSheet.create({
   },
   backgroundImage: {
     width: '100%',
-    height: 250,
+    height: 300,
   },
   rowMenu: {
     flexDirection: 'row',
@@ -75,5 +93,31 @@ const styles = StyleSheet.create({
   avatarImage: {
     height: '100%',
     width: '100%',
+  },
+  heroText: {
+    fontSize: 35,
+    fontWeight: '700',
+    color: 'white',
+    marginLeft: 25,
+  },
+  searchRow: {
+    flexDirection: 'row',
+    marginHorizontal: 25,
+    marginTop: 15,
+  },
+  searchPlaceholder: {
+    borderBottomColor: '#bbdefb',
+    borderBottomWidth: 1,
+    width: '80%',
+    marginLeft: 20,
+  },
+  searchText: {
+    color: '#bbdefb',
+    fontSize: 18,
+  },
+  linearGradient: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
   },
 });
