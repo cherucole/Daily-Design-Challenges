ad7a50e5101cdef28ec3f6443d233be388049b3f
Cheruiyot Collins <cheruyotcheruyot@gmail.com>
2020-04-20 17:16:49 +0300

    scrollview of thumbnails done

--- src/Streaming/screens/HomePage.js
+++ src/Streaming/screens/HomePage.js
@@ -11,6 +11,38 @@ import {
 import Entypo from 'react-native-vector-icons/Entypo';
 import Ionicons from 'react-native-vector-icons/Ionicons';
 import LinearGradient from 'react-native-linear-gradient';
+import { ScrollView } from 'react-native-gesture-handler';
+const SectionHeader = props => {
+  return (
+    <>
+      <Text style={styles.sectionText}>{props.name}</Text>
+      <View style={styles.lineDivider} />
+    </>
+  );
+};
+
+export const games = [
+  {
+    name: 'Apex Legends',
+    image: require('../assets/apex.jpg'),
+    views: '257.3k',
+  },
+  {
+    name: 'Overwatch',
+    image: require('../assets/overwatch.jpeg'),
+    views: '219.6k',
+  },
+  {
+    name: 'Fortnite',
+    image: require('../assets/fortnite.jpg'),
+    views: '177.3k',
+  },
+  {
+    name: 'Call of Duty',
+    image: require('../assets/cod.jpg'),
+    views: '107.3k',
+  },
+];
 
 const HomePage = props => {
   return (
@@ -22,7 +54,7 @@ const HomePage = props => {
         <LinearGradient
           ref={r => (this.gradiant = r)}
           locations={[0, 1.0]}
-          colors={['rgba(0,0,0,0.10)', 'rgba(0,0,0,0.80)']}
+          colors={['rgba(0,0,0,0.50)', 'rgba(0,0,0,0.80)']}
           style={styles.linearGradient}>
           <View style={styles.rowMenu}>
             <Entypo name="list" size={45} color="white" />
@@ -45,13 +77,38 @@ const HomePage = props => {
           </View>
         </LinearGradient>
       </ImageBackground>
+      <View style={styles.contentContainer}>
+        <SectionHeader name="Popular Categories" />
+        <View>
+          <ScrollView
+            horizontal
+            showsHorizontalScrollIndicator={false}
+            style={{ marginLeft: 10 }}>
+            {games.map(game => (
+              <View style={styles.thumbnails}>
+                <Image style={styles.thumbnailImage} source={game.image} />
+                <View>
+                  <Text style={styles.gameName}>{game.name}</Text>
+                  <View style={styles.metadata}>
+                    <Ionicons name="md-eye" size={24} color="#bbdefb" />
+                    <Text style={{ color: '#bbdefb' }}>
+                      {game.views} viewers
+                    </Text>
+                  </View>
+                </View>
+              </View>
+            ))}
+          </ScrollView>
+        </View>
+        <SectionHeader name="Top Live Streamers" />
 
-      <Text>HomePage</Text>
+        <Text>Yesss</Text>
+      </View>
 
-      <Button
+      {/* <Button
         title="Go To Details"
         onPress={() => props.navigation.navigate('Details')}
-      />
+      /> */}
     </View>
   );
 };
@@ -64,6 +121,11 @@ const styles = StyleSheet.create({
   backgroundImage: {
     width: '100%',
     height: 300,
+  },
+  linearGradient: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
   },
   rowMenu: {
     flexDirection: 'row',
@@ -99,6 +161,7 @@ const styles = StyleSheet.create({
     fontWeight: '700',
     color: 'white',
     marginLeft: 25,
+    marginTop: 2,
   },
   searchRow: {
     flexDirection: 'row',
@@ -115,9 +178,52 @@ const styles = StyleSheet.create({
     color: '#bbdefb',
     fontSize: 18,
   },
-  linearGradient: {
-    position: 'absolute',
+
+  sectionText: {
+    color: 'white',
+    fontSize: 19,
+    marginLeft: 25,
+    marginTop: 10,
+    fontWeight: '500',
+  },
+  lineDivider: {
+    backgroundColor: 'white',
+    height: 0.5,
+    width: '50%',
+    alignSelf: 'flex-end',
+    marginTop: -10,
+    marginBottom: 30,
+  },
+  contentContainer: {
+    flex: 1,
+    backgroundColor: '#4651FF',
+    borderTopRightRadius: 40,
+    marginTop: -40,
+    paddingTop: 20,
+  },
+  thumbnails: {
+    backgroundColor: '#546BFF',
+    marginHorizontal: 8,
+    height: 250,
+    width: 150,
+    borderRadius: 5,
+    overflow: 'hidden',
+  },
+  thumbnailImage: {
+    height: '77%',
     width: '100%',
-    height: '100%',
+  },
+  gameName: {
+    marginLeft: 10,
+    marginTop: 5,
+    fontSize: 16,
+    fontWeight: '700',
+    color: 'white',
+  },
+  metadata: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginHorizontal: 10,
+    justifyContent: 'space-around',
   },
 });
