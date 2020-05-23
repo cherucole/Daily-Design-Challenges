1d08954ed33ae9d7e4eb23d6d3ac917de3b4eea8
Cheruiyot Collins <cheruyotcheruyot@gmail.com>
2020-05-23 17:00:00 +0300

    styling with minimal inline achieved

--- src/Netflix/screens/Home.js
+++ src/Netflix/screens/Home.js
@@ -16,30 +16,16 @@ const Home = props => {
           end={{ x: 0, y: 1.3 }}
           locations={[0.1, 0.5]}
           style={styles.linearGradient}>
-          <View
-            style={{
-              paddingTop: 60,
-              paddingHorizontal: 20,
-              flexDirection: 'row',
-              justifyContent: 'space-between',
-            }}>
-            <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <View style={styles.header}>
+            <View style={styles.headerLeft}>
               <MaterialIcons
                 name="keyboard-backspace"
                 size={28}
                 color="#F71131"
               />
-              <Text
-                style={{
-                  color: 'white',
-                  fontSize: 16,
-                  fontWeight: '700',
-                  marginLeft: 10,
-                }}>
-                Home
-              </Text>
+              <Text style={styles.headerText}>Home</Text>
             </View>
-            <View style={{ flexDirection: 'row' }}>
+            <View style={styles.headerRight}>
               <Feather name="heart" size={24} color="white" />
               <Feather
                 name="share"
@@ -49,56 +35,25 @@ const Home = props => {
               />
             </View>
           </View>
-          <View style={{ marginTop: 470 }}>
-            <Text
-              style={{
-                color: 'white',
-                fontSize: 14,
-                fontWeight: '700',
-                alignSelf: 'center',
-              }}>
-              NEW - SEASON 1
-            </Text>
-            <Text
-              style={{
-                fontSize: 30,
-                fontWeight: '800',
-                color: 'white',
-                alignSelf: 'center',
-                marginVertical: 15,
-                transform: [{ scaleY: 1.8 }], //changes items to be longer in height
-              }}>
-              THE LAST DANCE
-            </Text>
-            <View
-              style={{
-                flexDirection: 'row',
-                marginHorizontal: 68,
-                justifyContent: 'space-between',
-                marginTop: 15,
-              }}>
+          <View style={styles.content}>
+            <Text style={styles.newSeasonText}>NEW - SEASON 1</Text>
+            <Text style={styles.movieName}>THE LAST DANCE</Text>
+            <View style={styles.metadataContainer}>
               <View style={styles.movieMetadata}>
-                <Text style={{ fontWeight: '600' }}>Popular with friends</Text>
+                <Text style={styles.metaText}>Popular with friends</Text>
               </View>
               <View style={styles.movieMetadata}>
-                <Text style={{ fontWeight: '600' }}>12+</Text>
+                <Text style={styles.metaText}>12+</Text>
               </View>
               <View
                 style={{ ...styles.movieMetadata, backgroundColor: '#FFB34B' }}>
-                <Text style={{ fontWeight: '900' }}>
-                  IMDb <Text style={{ fontWeight: '600' }}>9.4</Text>
+                <Text style={styles.imdb}>
+                  IMDb <Text style={styles.metaText}>9.4</Text>
                 </Text>
               </View>
             </View>
             <View>
-              <Text
-                style={{
-                  fontSize: 13,
-                  color: 'white',
-                  fontWeight: '600',
-                  alignSelf: 'center',
-                  marginVertical: 15,
-                }}>
+              <Text style={styles.tags}>
                 {`2020  •  Documentary  •  Biography`}
               </Text>
             </View>
@@ -110,35 +65,11 @@ const Home = props => {
               ]}
               start={{ x: 0, y: 0 }}
               end={{ x: 1, y: 0 }}
-              style={{
-                height: 2,
-                marginTop: 5,
-                marginBottom: 20,
-                width: 300,
-                alignSelf: 'center',
-              }}
+              style={styles.breaker}
             />
-            <View
-              style={{
-                backgroundColor: '#F71131',
-                width: 160,
-                flexDirection: 'row',
-                justifyContent: 'center',
-                alignItems: 'center',
-                height: 34,
-                alignSelf: 'center',
-                borderRadius: 2,
-              }}>
+            <View style={styles.nextButton}>
               <MaterialIcons name="play-arrow" size={24} color="white" />
-              <Text
-                style={{
-                  color: 'white',
-                  fontSize: 15,
-                  fontWeight: '800',
-                  marginLeft: 5,
-                }}>
-                Next Episode
-              </Text>
+              <Text style={styles.nextSeasonText}>Next Episode</Text>
             </View>
           </View>
         </LinearGradient>
@@ -160,9 +91,87 @@ const styles = StyleSheet.create({
     width: '100%',
     height: '100%',
   },
+  header: {
+    paddingTop: 60,
+    paddingHorizontal: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  headerText: {
+    color: 'white',
+    fontSize: 16,
+    fontWeight: '700',
+    marginLeft: 10,
+  },
+  headerLeft: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  headerRight: {
+    flexDirection: 'row',
+  },
+  content: {
+    marginTop: 470,
+  },
+  newSeasonText: {
+    color: 'white',
+    fontSize: 14,
+    fontWeight: '700',
+    alignSelf: 'center',
+  },
+  movieName: {
+    fontSize: 30,
+    fontWeight: '800',
+    color: 'white',
+    alignSelf: 'center',
+    marginVertical: 15,
+    transform: [{ scaleY: 1.8 }], //changes items to be longer in height
+  },
+  metadataContainer: {
+    flexDirection: 'row',
+    marginHorizontal: 68,
+    justifyContent: 'space-between',
+    marginTop: 15,
+  },
   movieMetadata: {
     backgroundColor: 'white',
     padding: 5,
     borderRadius: 2,
+  },
+  imdb: {
+    fontWeight: '900',
+  },
+  tags: {
+    fontSize: 13,
+    color: 'white',
+    fontWeight: '600',
+    alignSelf: 'center',
+    marginVertical: 15,
+  },
+  breaker: {
+    height: 2,
+    marginTop: 5,
+    marginBottom: 20,
+    width: 300,
+    alignSelf: 'center',
+  },
+  nextButton: {
+    backgroundColor: '#F71131',
+    width: 160,
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: 34,
+    alignSelf: 'center',
+    borderRadius: 2,
+  },
+  nextSeasonText: {
+    color: 'white',
+    fontSize: 15,
+    fontWeight: '800',
+    marginLeft: 5,
+  },
+  metaText: {
+    fontWeight: '600',
   },
 });
