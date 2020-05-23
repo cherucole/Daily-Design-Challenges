7d52eefa82895ddfe2eda5d8aba700aecb0c9d29
Cheruiyot Collins <cheruyotcheruyot@gmail.com>
2020-05-23 15:09:39 +0300

    added background image and gradient

--- src/Netflix/screens/Home.js
+++ src/Netflix/screens/Home.js
@@ -1,17 +1,37 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground } from 'react-native';
+import LinearGradient from 'react-native-linear-gradient';
 
-const Home = props => (
-  <View style={styles.container}>
-    <Text>Home</Text>
-  </View>
-);
+const Home = props => {
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        source={require('../assets/michael.jpg')}
+        style={styles.imageBackground}>
+        <LinearGradient
+          colors={['rgba(0,0,0,0.2)', 'rgba(8, 9, 12,1)']}
+          start={{ x: 0, y: 0.4 }}
+          end={{ x: 0, y: 1.3 }}
+          locations={[0.1, 0.5]}
+          style={styles.linearGradient}>
+          <Text>Home</Text>
+        </LinearGradient>
+      </ImageBackground>
+    </View>
+  );
+};
 export default Home;
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  imageBackground: {
+    flex: 1,
+  },
+  linearGradient: {
+    position: 'absolute',
+    width: '100%',
+    height: '100%',
   },
 });
